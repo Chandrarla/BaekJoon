4a6c452367fd54e05e894fefb6ea2fafa9e97162
Chandrala <gudrms2538@gmail.com>
2023-04-06 17:13:11 +0900

    .

--- lv2/9498.py
+++ lv2/9498.py
@@ -1,11 +1,11 @@
 a = int(input())
 if(90<=a<=100):
     print('A')
-if(80<=a<=89):
+elif(80<=a<=89):
     print('B')
-if(70<=a<=79):
+elif(70<=a<=79):
     print('C')
-if(60<=a<=69):
+elif(60<=a<=69):
     print('D')
-if(a<60):
+else:
     print('F')
