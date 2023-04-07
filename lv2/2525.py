902e899da77e1d4ff5cf31d6b7644f5b1b878424
Chandrala <gudrms2538@gmail.com>
2023-04-07 15:21:21 +0900

    .

---
+++ lv2/2525.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+c = int(input())
+
+min = b + c
+hour = a + (min // 60)
+
+if min > 59 and hour < 23:
+    print(hour, min % 60)
+elif min > 59 and hour > 23:
+    print(hour % 24, min % 60)
+else:
+    print(a, min)
