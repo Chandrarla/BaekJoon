c178a6036f0f6e7ae9c463082f49d5b69562ba77
Chandrala <gudrms2538@gmail.com>
2023-04-07 15:23:27 +0900

    .

--- lv2/2525.py
+++ lv2/2525.py
@@ -6,7 +6,7 @@ hour = a + (min // 60)
 
 if min > 59 and hour < 23:
     print(hour, min % 60)
-elif min > 59 and hour > 23:
+elif min > 59 and hour >= 23:
     print(hour % 24, min % 60)
 else:
     print(a, min)
