a45a6e1add0fbded6505f3ad90a50ea1308bd745
Chandrala <gudrms2538@gmail.com>
2023-04-08 20:12:41 +0900

    .

--- lv2/2480.py
+++ lv2/2480.py
@@ -1,11 +1,12 @@
 num = list(map(int, input().split()))
+print(num)
 num.sort()
-
-if num[0] == num[1] == num[2]:
-    print(10000 + num[0] * 1000)
-elif num[0] == num[1] or num[1] == num[2]:
-    print(1000 + num[1] * 100)
-else:
-    print(num[2] * 100)
+#
+# if num[0] == num[1] == num[2]:
+#     print(10000 + num[0] * 1000)
+# elif num[0] == num[1] or num[1] == num[2]:
+#     print(1000 + num[1] * 100)
+# else:
+#     print(num[2] * 100)
 
 # input() 후 .sort()로 크기 순으로 정렬
