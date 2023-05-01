e1bb288183f71f9eeba2b34bfa6af98805ccdfe0
Chandrala <gudrms2538@gmail.com>
2023-05-01 15:16:48 +0900

    .

---
+++ lv6/1157.py
@@ -0,0 +1,13 @@
+text = input().upper()
+uniText = list(set(text))
+
+cntList = []
+for i in uniText:
+    cnt = text.count(i)
+    cntList.append(cnt)
+
+if cntList.count(max(cntList)) > 1 :
+    print("?")
+else :
+    maxIndex = cntList.index(max(cntList))
+    print(uniText[maxIndex])
